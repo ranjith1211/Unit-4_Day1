5156e1255478fded6dab225b2a3a7dd7dc368bd1
ranjith1211 <ranjithacharya1211@gmail.com>
2022-03-06 22:41:47 +0530

    Day-1

---
+++ main.js
@@ -0,0 +1,10 @@
+const add = require("./add");
+const divide = require("./divide");
+const multiply = require("./multiply");
+const subtract = require("./subtract");
+var a=10;
+var b=5;
+console.log("Addition: "+add.add(a,b))
+console.log("Addition: "+divide.divide(a,b))
+console.log("Addition: "+multiply.multiply(a,b))
+console.log("Addition: "+subtract.substract(a,b))
